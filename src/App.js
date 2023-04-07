82d4869d05541930795a76cbfabc1d96f8aea5c2
Lawal Oluwatobi <lawaloluwatobi128@gmail.com>
2023-04-08 00:47:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Carousel from "./Carousel/Carousel";
 import "./styles.scss";
 import {
   createBrowserRouter,
@@ -10,14 +11,17 @@ import {
 const Root = () => {
   return (
     <div>
-      <h1>Root</h1>
       <Outlet />
     </div>
   );
 };
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Root />}></Route>)
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="/" element={<Carousel />} />
+    </Route>
+  )
 );
 
 const App = () => {
